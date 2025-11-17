5accc59ac285326850b78f1f540efbe4f318f7ae
Ivan Jörg <i.joerg1977@gmail.com>
2025-11-17 08:22:09 +0100

    MB004 neu

---
+++ MB04/A9-2-3/Form1.cs
@@ -0,0 +1,10 @@
+namespace A9_2_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MB04/A9-2-4/Form1.cs
@@ -0,0 +1,10 @@
+namespace A9_2_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
